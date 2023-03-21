ea70703c18f6ad9b9e3f6d40ce9f682b1962bbbb
Jesús Enrique Rascón <blograso@gmail.com>
2023-03-21 23:58:07 +0100

    lmao

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+
+	let a = 0;
+
+	for (let i = 0; i < 10; i++) {
+		a += 2;
+
+		console.log(a);
+	}
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	let scale = 35;
+	let textOffset = 20;
+</script>
+
+<div class="flex justify-center">
+	<svg width="500" height="500" viewBox="-50 -50 100 100">
+		{#each [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55] as minute, hour}
+			<line class="major stroke-stone-500" y1="35" y2="45" transform="rotate({30 * minute})" />
+			<text
+				class="fill-stone-600 font-serif"
+				text-anchor="middle"
+				font-size="8"
+				dominant-baseline="middle"
+				transform="translate({-Math.sin(textOffset * minute) * scale}, {-Math.cos(
+					textOffset * minute
+				) * scale})">{((hour + 11) % 12) + 1}</text
+			>
+			{#each [1, 2, 3, 4] as offset}
+				<line
+					class="minor stroke-stone-300"
+					y1="40"
+					y2="45"
+					transform="rotate({6 * (minute + offset)})"
+				/>
+			{/each}
+		{/each}
+	</svg>
+</div>
